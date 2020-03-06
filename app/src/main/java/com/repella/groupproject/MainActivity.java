f7c281cddb5b9da1c535dfc797514388a8f0c122
JonHendrickson <jonhen11@hotmail.com>
2020-03-06 15:22:36 -0500

    Fixed authentication does not handle no input well will crash

--- app/src/main/java/com/repella/groupproject/MainActivity.java
+++ app/src/main/java/com/repella/groupproject/MainActivity.java
@@ -2,12 +2,16 @@ package com.repella.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.repella.groupproject.data.Location;
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final EditText userName = (EditText) findViewById(R.id.username);
         final EditText password = (EditText) findViewById(R.id.password);
-       // final Snackbar mySnackbar = Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Login Falied", Snackbar.LENGTH_LONG);
+      // final Snackbar mySnackbar = Snackbar.make(findViewById(R.id.), "Login Falied", Snackbar.LENGTH_LONG);
 
 
         //Create the database.
@@ -67,18 +71,28 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                User userdb= dbm.selectUser(userName.getText().toString());
-                if (password.getText().toString().equals(userdb.getPassword())){
-                    Intent i = new Intent(getApplicationContext(), UserLanding.class);
+                String username = userName.getText().toString();
+                if(username==" ") {
+                    Toast.makeText(getApplicationContext(), "You forgot your username",
+                            Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(i);
-                }else{
-                //    mySnackbar.show();
+                }else {
+                    User userdb = dbm.selectUser(userName.getText().toString());
+                    if (password.getText().toString().equals(userdb.getPassword())) {
+                        Toast.makeText(getApplicationContext(), "Login Succes",
+                                Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(getApplicationContext(), UserLanding.class);
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Login Failed",
+                                Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
                 }
-
-
-                Intent i = new Intent(getApplicationContext(), UserLanding.class);
-                startActivity(i);
             }
         });
     }
+
 }
